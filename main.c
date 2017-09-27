c37dd6d8dcb24b96d2def3ae2223dd8be3195d0f
Kasparjj <kaspar_jj@hotmail.com>
2017-09-27 14:55:00 +0200

    -

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 
 int main()
 {
-    int Myj;
+    int My;
 
     QRS_params qsr_params;                   // Instance of the made available through: #include "qsr.h"
     openfile("ECG.txt");                     // Pointer to a file object;
