49057b757be699750390cc009c2d2a1d380c4fd5
JKThorning <jkthorning@gmail.com>
2017-09-27 09:47:51 +0200

    cmake build and first run

--- cmake-build-debug/CMakeFiles/cn.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/cn.dir/DependInfo.cmake
@@ -4,12 +4,12 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/Users/Kaspar/CLionProjects/cn/filters.c" "/Users/Kaspar/CLionProjects/cn/cmake-build-debug/CMakeFiles/cn.dir/filters.c.o"
-  "/Users/Kaspar/CLionProjects/cn/main.c" "/Users/Kaspar/CLionProjects/cn/cmake-build-debug/CMakeFiles/cn.dir/main.c.o"
-  "/Users/Kaspar/CLionProjects/cn/qsr.c" "/Users/Kaspar/CLionProjects/cn/cmake-build-debug/CMakeFiles/cn.dir/qsr.c.o"
-  "/Users/Kaspar/CLionProjects/cn/sensor.c" "/Users/Kaspar/CLionProjects/cn/cmake-build-debug/CMakeFiles/cn.dir/sensor.c.o"
+  "C:/Users/Jacob/Google Drive/Dokumenter/Studie/3 semester/02132 Computer systems E17/Assignment 1/CLion/computersystems_assignment_1/filters.c" "C:/Users/Jacob/Google Drive/Dokumenter/Studie/3 semester/02132 Computer systems E17/Assignment 1/CLion/computersystems_assignment_1/cmake-build-debug/CMakeFiles/cn.dir/filters.c.obj"
+  "C:/Users/Jacob/Google Drive/Dokumenter/Studie/3 semester/02132 Computer systems E17/Assignment 1/CLion/computersystems_assignment_1/main.c" "C:/Users/Jacob/Google Drive/Dokumenter/Studie/3 semester/02132 Computer systems E17/Assignment 1/CLion/computersystems_assignment_1/cmake-build-debug/CMakeFiles/cn.dir/main.c.obj"
+  "C:/Users/Jacob/Google Drive/Dokumenter/Studie/3 semester/02132 Computer systems E17/Assignment 1/CLion/computersystems_assignment_1/qsr.c" "C:/Users/Jacob/Google Drive/Dokumenter/Studie/3 semester/02132 Computer systems E17/Assignment 1/CLion/computersystems_assignment_1/cmake-build-debug/CMakeFiles/cn.dir/qsr.c.obj"
+  "C:/Users/Jacob/Google Drive/Dokumenter/Studie/3 semester/02132 Computer systems E17/Assignment 1/CLion/computersystems_assignment_1/sensor.c" "C:/Users/Jacob/Google Drive/Dokumenter/Studie/3 semester/02132 Computer systems E17/Assignment 1/CLion/computersystems_assignment_1/cmake-build-debug/CMakeFiles/cn.dir/sensor.c.obj"
   )
-set(CMAKE_C_COMPILER_ID "AppleClang")
+set(CMAKE_C_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
